13434915ecc024fc2c98d202d525c22ac5937047
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-28 10:05:46 -0300

    Added all actions to the controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,27 @@ class BooksController < ApplicationController
     
     def index
     end
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def edit 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end
+
+    private 
+    def books_params 
+    end 
+
+    def set_book
+    end
+
 end
