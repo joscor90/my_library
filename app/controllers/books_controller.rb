ff07895064a3b174b33a570795f1000f8db88fd2
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-28 10:46:49 -0300

    Added ransack gem and modified inde action in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,8 @@ class BooksController < ApplicationController
     before_action :set_book, only: [:show, :edit, :update, :destroy]
     
     def index
-        @books = Book.all
+        @q = Book.ransack(params[:q])
+        @books = @q.result(distinct: :true)
     end
 
     def show
